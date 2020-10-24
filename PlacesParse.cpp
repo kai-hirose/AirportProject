109b6277a86082c8c03f7822eda5e35601600791
Shaun Lee <shaunleeyx@gmail.com>
2020-10-23 23:49:48 -0700

    done parsing state and city

--- PlacesParse.cpp
+++ PlacesParse.cpp
@@ -10,23 +10,29 @@ int main()
         ifstream myfile("places\\places2k.txt");
         if (myfile.is_open())
         {
-                stringstream ss;
                 string city;
+                string state;
                 string lat;
                 string lon;
                 string temp;
+                stringstream ss;
                 while (getline(myfile, line))
                 {
+                        string citywithstate = "";
+                        city = "";
+                        state = "";
                         ss.str(line);
-                        string temp;
                         while(ss >> temp)
                         {
-                                // if(isalpha(temp[1])){
-                                //         city.append(temp + " ");
-                                // }
-                                
-                                cout << temp << endl;
+                                if(isalpha(temp[1])) citywithstate.append(temp + " "); 
                         }
+                        state = citywithstate.substr(0,2);
+                        city = citywithstate.substr(9,citywithstate.length());
+                        ss.seekg(5);
+                        ss >> lat;
+                        ss >> lon;
+                        cout << "state:" << state + " city:" + city + " lat:" + lat + " lon:" + lon << endl;
+                        ss.clear();
                 }
         }
         myfile.close();
