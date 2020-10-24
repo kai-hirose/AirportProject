3597c19f37f3aa623ff767d7c20550ffc19b595b
Shaun Lee <shaunleeyx@gmail.com>
2020-10-24 00:34:17 -0700

    finished parsing

--- PlacesParse.cpp
+++ PlacesParse.cpp
@@ -5,36 +5,43 @@ using namespace std;
 
 int main()
 {
-        string line;
-        int count = 0;
-        ifstream myfile("places\\places2k.txt");
-        if (myfile.is_open())
+    string line;
+    ifstream myfile("places//places2k.txt");
+    if (myfile.is_open())
+    {
+        string city;
+        string state;
+        string lat;
+        string lon;
+        string temp;
+        stringstream ss;
+        while (getline(myfile, line))
         {
-                string city;
-                string state;
-                string lat;
-                string lon;
-                string temp;
-                stringstream ss;
-                while (getline(myfile, line))
+            int count = 0;
+            string citywithstate = "";
+
+            ss.str(line);
+            while(ss >> temp)
+            {
+                if(isalpha(temp[1])) 
                 {
-                        string citywithstate = "";
-                        city = "";
-                        state = "";
-                        ss.str(line);
-                        while(ss >> temp)
-                        {
-                                if(isalpha(temp[1])) citywithstate.append(temp + " "); 
-                        }
-                        state = citywithstate.substr(0,2);
-                        city = citywithstate.substr(9,citywithstate.length());
-                        ss.seekg(5);
-                        ss >> lat;
-                        ss >> lon;
-                        cout << "state:" << state + " city:" + city + " lat:" + lat + " lon:" + lon << endl;
-                        ss.clear();
+                    citywithstate.append(temp + " "); 
+                    count = 0; 
                 }
+                if(count ==  7) lat = temp; 
+                if(count == 8) lon = temp;
+                count++;
+
+            }
+            state = citywithstate.substr(0,2);
+            city = citywithstate.substr(9,citywithstate.length());
+            ss.seekg(5);
+            ss >> lat;
+            ss >> lon;
+            cout << "state:" << state + " city:" + city + " lat:" + lat + " lon:" + lon << endl;
+            ss.clear();
         }
-        myfile.close();
-        return 0;
+    }
+    myfile.close();
+    return 0;
 }
