21c7e79ed76ef0ce69cbb861b0faba0b5bf7e704
kai-hirose <60566825+kai-hirose@users.noreply.github.com>
2020-10-25 16:11:36 -0700

    Update PlacesParse.cpp

--- PlacesParse.cpp
+++ PlacesParse.cpp
@@ -1,15 +1,16 @@
 #include <string>
-#include <iostream>
-#include <sstream>
 #include <fstream>
-
+#include <sstream>
+#include <iostream>
 using namespace std;
 
 class trie {
 public:
     trie() {
+        tree = new tNode;
+        tree->array = nullptr;
         string line;
-        ifstream myfile("places\\places2k.txt");
+        ifstream myfile("places/places2k.txt");
         if (myfile.is_open()) {
             string city;
             string state;
@@ -37,68 +38,81 @@ public:
                 //Extract state and city from the text we saved earlier.
                 state = citywithstate.substr(0, 2);
                 city = citywithstate.substr(9, citywithstate.length());
-                //cout << "state:" << state + " city:" + city + " lat:" + lat + " lon:" + lon << endl;
-                string properName = state + city;
-                tNodePtr* tempPtr = &tree;
+                cout << "state:" << state + " city:" + city + " lat:" + lat + " lon:" + lon << endl;
+                string stateCitySpaces = city + state;
+                string properName;
+                for (int i = 0; i < stateCitySpaces.length(); i++) {
+                    if (stateCitySpaces[i] != ' ' && isalpha(stateCitySpaces[i])) {
+                        //Gets rid of special chars
+                        properName += toupper(stateCitySpaces[i]);
+                    }
+                }
+                tNodePtr temp = tree;
                 for (int i = 0; i < properName.length(); i++) {
-                    //If the ptr is not allocated, it is the end of the branch so we must allocate it.
-                    if ((*tempPtr) = nullptr) {
-                        (*tempPtr) = new tNode;
-                        //Get index of the char (case insensitive) and set it to index
-                        (*tempPtr)->array = new tNode[26];
+                    //If the node'a array is not allocated.
+                    if (temp->array = nullptr) {
+                        cout << ((int)properName[i]) - 65 << endl;
+                        temp->index = ((int)properName[i]) - 65;
+                        temp->lat = std::stod(lat);
+                        temp->lon = std::stod(lon);
+                        temp->array = new tNode[26];
                         for (int i = 0; i < 26; i++) {
-                            (*tempPtr)->array[i] = nullptr;
+                            temp->array[i].array = nullptr;
                         }
-                        (*tempPtr)->index = ((int)toupper(properName[i])) - 65;
-                        //Set lat and lon, move the ptr to the next "letter"
-                        (*tempPtr)->lat = std::stod(lat);
-                        (*tempPtr)->lon = std::stod(lon);
-                        (*tempPtr) = &((*tempPtr)->array[i]);
+                        temp = &(temp->array[temp->index]);
                     }
                     else {
                         //If the index of the current ptr is not -1, it means there was only one "branch" below it.
                         //Now there will be more than one opion available so we need to set index to -1 to signify it.
-                        if ((*tempPtr)->index != -1) {
-                            (*tempPtr)->index = -1;
+                        if (temp->index != -1) {
+                            temp->index = -1;
                         }
-                        (*tempPtr) = &((*tempPtr)->array[((int)toupper(properName[i])) - 65]);
+                        temp = &(temp->array[((int)properName[i]) - 65]);
                     }
                 }
                 ss.clear();
             }
+            myfile.close();
         }
-        myfile.close();
+        cout << "Tree built." << endl;
+        if (tree == nullptr)
+            cout << "fail" << endl;
     }
 
     coordinateCP search(string name) {
-        tNodePtr tempPtr = tree;
+        tNodePtr temp = tree;
         coordinateCP returnVal;
         for (int i = 0; i < name.length(); i++) {
+            cout << name[i] << endl;
+            if (tree == nullptr) {
+                cout << "failed" << endl;
+            }
             //Invalid letter returns an invalid latitude that the main program checks.
             if (!isalpha(name[i])) {
                 returnVal.lat = -999;
                 return returnVal;
             }
-            //Return the lat and lon stored in the node if index is not -1
-            //-1 indicates there is more than one branch beneath.
-            if (tempPtr->index != -1) {
-                returnVal.lat = tempPtr->lat;
-                returnVal.lon = tempPtr->lon;
-                return returnVal;
-            }
 
-            if (tempPtr->array[((int)toupper(name[i])) - 65] == nullptr) {
+            if (temp->array = nullptr) {
                 //If there is letters left and the next node is null, return invalid lat.
                 if (i < name.length() - 1) {
                     returnVal.lat = -999;
                     return returnVal;
                 }
                 //If there is no letters left and the next node is null, return the lat and lat stored here.
-                returnVal.lat = tempPtr->lat;
-                returnVal.lon = tempPtr->lon;
+                returnVal.lat = temp->lat;
+                returnVal.lon = temp->lon;
                 return returnVal;
             }
-            tempPtr->tempPtr->array[((int)toupper(name[i])) - 65];
+
+            //Return the lat and lon stored in the node if index is not -1
+            //-1 indicates there is more than one branch beneath.
+            if (temp->index != -1) {
+                returnVal.lat = temp->lat;
+                returnVal.lon = temp->lon;
+                return returnVal;
+            }
+            temp = &(temp->array[((int)toupper(name[i])) - 65]);
         }
 
         //If we havent returned anything somehow, return invalid lat.
@@ -107,14 +121,5 @@ public:
     }
 
 private:
-    tNodePtr tree = nullptr;
+    tNodePtr tree;
 };
-
-int
-main(int argc, char* argv[])
-{
-    trie myTrie;
-    coordinateCP test;
-    test = myTrie.search("SeAttleWA");
-    cout << test.lat << " " << test.lon;
-    return 0;
